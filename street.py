6fdc3cc656c1bb8afd307eda646ee2306dad5dc5
Ilnur Nagametyanov <ilnur.nagametyanov@yandex.ru>
2023-05-25 22:57:34 +0500

    первое происхождение

---
+++ street.py
@@ -0,0 +1,9 @@
+while True:
+
+    x = int(input('Введите число: '))
+    y = int(input('Введите второе число: '))
+    c = int(input('Что делаем 1/2 ? '))
+    if c == 1:
+        print('Ответ', x * y)
+    elif c == 2:
+        print('Ответ', x + y)
