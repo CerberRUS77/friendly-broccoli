1e694348c8faa216d4a28c6b3104a02becb24895
Ilnur Nagametyanov <ilnur.nagametyanov@yandex.ru>
2023-06-01 22:06:02 +0500

    искусственный конфликт

--- street.py
+++ street.py
@@ -2,13 +2,13 @@ while True:
 
     x = int(input('Введите число: '))
     y = int(input('Введите второе число: '))
-    c = int(input('Что делаем 1/2/3 ? '))
+    c = int(input('Что делаем 1/2/4 ? '))
     if c == 1:
         print('Ответ', x * y)
     elif c == 2:
         print('Ответ', x + y)
     else:
-        c == 3
+        c == 4
         print('Закончили)')
         break
 
