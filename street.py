0410ee191aaac2d6c6a6da99688952bfed2ee818
Ilnur Nagametyanov <ilnur.nagametyanov@yandex.ru>
2023-06-01 22:24:07 +0500

    оканчание конфликта

--- street.py
+++ street.py
@@ -2,13 +2,13 @@ while True:
 
     x = int(input('Введите число: '))
     y = int(input('Введите второе число: '))
-    c = int(input('Что делаем 1/2/4 ? '))
+    c = int(input('Что делаем 1/2/3 ? '))
     if c == 1:
         print('Ответ', x * y)
     elif c == 2:
         print('Ответ', x + y)
     else:
-        c == 4
+        c == 3
         print('Закончили)')
         break
 
